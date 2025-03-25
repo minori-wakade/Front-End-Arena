33da3710b3fef4e262e7aba67883f190d28c89ef
Minori Wakade <minoriwakade@gmail.com>
2025-03-25 08:04:53 +0530

    Seventh commit

--- detailsscript.js
+++ detailsscript.js
@@ -1,18 +1,16 @@
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("Website loaded successfully!");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Website loaded successfully!");
 });
 const navToggle = document.getElementById("navToggle");
 const navLinks = document.getElementById("navLinks");
 
 navToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
+  navLinks.classList.toggle("show");
 });
 document.querySelector(".confirm-btn:nth-child(1)").addEventListener("click", function() {
-    alert("Edit Details clicked!");
-  });
-  
-  document.querySelector(".confirm-btn:nth-child(2)").addEventListener("click", function() {
-    alert("Details Confirmed!");
-  });
-  
+  alert("Edit Details clicked!");
+});
+
+document.querySelector(".confirm-btn:nth-child(2)").addEventListener("click", function() {
+  alert("Details Confirmed!");
+});
